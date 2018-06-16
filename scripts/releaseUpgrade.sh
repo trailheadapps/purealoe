a632af6e916c04bd9f38ab36763b1d118932e15a
muenzpraeger <rwinkelmeyer@salesforce.com>
2018-06-16 20:38:10 +0200

    Adding release test script / updating orgInit script

--- scripts/releaseUpgrade.sh
+++ scripts/releaseUpgrade.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-DURATION=7
-
-if [ "$#" -eq 1 ]; then
-  DURATION=$1
-fi
-
-sfdx force:org:create -a purealoe -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:project:upgrade -f
+sfdx muenzpraeger:source:api:set
+sfdx force:org:create -a purealoe-release-test -s -f config/project-scratch-def.json -d 7
 sfdx force:source:push
 sfdx force:user:permset:assign -n purealoe
 sfdx force:data:tree:import --plan ./data/Harvest_Field__c-plan.json
 sfdx force:data:tree:import --plan ./data/Merchandise__c-plan.json
+sfdx force:apex:test:run
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
