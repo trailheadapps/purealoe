db709f95b940f72efe31e31939848d93ea098806
muenzpraeger <rwinkelmeyer@salesforce.com>
2018-10-09 07:06:30 +0200

    Fixed deprecated SLDS BEM syntax

--- force-app/main/default/aura/expandTemplate/expandTemplateController.js
+++ force-app/main/default/aura/expandTemplate/expandTemplateController.js
@@ -3,7 +3,7 @@
         component.set("v.fullScreen", !component.get("v.fullScreen"));
         $A.util.toggleClass(component.find("container"), 'fullscreen');
         var leftColumn = component.find("leftColumn");
-        $A.util.toggleClass(leftColumn, 'slds-size--6-of-12');
-        $A.util.toggleClass(leftColumn, 'slds-size--12-of-12');
+        $A.util.toggleClass(leftColumn, 'slds-size_6-of-12');
+        $A.util.toggleClass(leftColumn, 'slds-size_12-of-12');
     }
 })
